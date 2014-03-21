a1172ea7460d969bd06d4c903f9427b1a0792430
Steven Lybeck <slybeck@gmail.com>
2014-03-21 14:05:50 -0700

    Prolog - Day 1, demos and self-study.

---
+++ prolog/1/1-favorite-books.pl
@@ -0,0 +1,5 @@
+book(vonnegut, slaughterhouse_five).
+book(tate, seven_languages_seven_weeks).
+book(vonnegut, deadeye_dick).
+book(eggers, heartbreaking_work_of_staggering_genuis).
+book(eggers, you_shall_know_our_velocity).
---
+++ prolog/1/2-musicians.pl
@@ -0,0 +1,18 @@
+plays(hendrix, guitar).
+plays(clapton, guitar).
+plays(wonder, piano).
+plays(kenny_g, saxophone).
+plays(morissette, harmonica).
+plays(morissette, guitar).
+plays(ulrich, drums).
+
+genre(hendrix, rock).
+genre(clapton, rock).
+genre(wonder, r_and_b).
+genre(kenny_g, smooth_jazz).
+genre(morissette, alternative).
+genre(ulrich, metal).
+
+
+% idea: genre and subgenre mappings!
+% 	then you can query something like What instruments are played in the rock genre? And it will include rock, alternative, metal, etc...
